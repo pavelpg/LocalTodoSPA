7e01c4ac463f4423339ff858bce96b7831392ad2
Pavel <>
2017-01-20 22:11:53 +1000

    initial revision

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TodoApplication = require('./todo-spa-component.js');
+
+ReactDOM.render(<TodoApplication />, document.getElementById('app'));
+
+
